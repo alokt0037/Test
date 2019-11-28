6982c3372605b9b65c3634809070fa57455bb424
n452499 <alokt0037@gmail.com>
2019-11-28 15:31:12 +0530

    First commit Message

---
+++ src/main/java/com/example/DemoJenG/DemoJenGApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoJenG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenGApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DemoJenG/DemoJenGApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoJenG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
